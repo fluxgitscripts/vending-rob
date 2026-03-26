bb7f24ec873f4f27f155d695ccc252efb69d4e5c
fluxgitscripts <zzkxnsti@gmail.com>
2026-03-26 17:56:35 +0100

    Update .lua

--- .lua
+++ .lua
@@ -1,5 +1,3 @@
-local SCRIPT_URL = "https://raw.githubusercontent.com/fluxgitscripts/vending-rob/refs/heads/main/.lua"
-
 local Players             = game:GetService("Players")
 local TweenService        = game:GetService("TweenService")
 local VirtualInputManager = game:GetService("VirtualInputManager")
@@ -432,7 +430,7 @@ local function flyUpAndHop()
     if queue_on_teleport then
         local payload = [[
             wait(3)
-            loadstring(game:HttpGet("]] .. SCRIPT_URL .. [["))()
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/fluxgitscripts/vending-rob/refs/heads/main/.lua"))()
         ]]
         pcall(function() queue_on_teleport(payload) end)
     end
